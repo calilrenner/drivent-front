0c5395b700c8a8d260c3c4d6db1baad8e33398c1
okitauehara <marcosokita@live.com>
2022-01-19 19:44:32 -0300

    paymentAccess: conditional render and loading component implemented

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,23 +1,47 @@
 import styled from "styled-components";
+import { useState, useEffect } from "react";
+import useApi from "../../../hooks/useApi";
+import Loading from "../../../components/Loading";
 
 export default function Payment() {
+  const [hasSubscription, setHasSubscription] = useState("");
+  const [loading, setLoading] = useState(false);
+  const { enrollment } = useApi();
+
+  useEffect(() => { 
+    setLoading(true);
+    enrollment.getPersonalInformations()
+      .then((response) => {
+        if (response.status === 200) {
+          setHasSubscription(true);
+          setLoading(false);
+        } else {
+          setHasSubscription(false);
+          setLoading(false);
+        }
+      });
+  }, [hasSubscription]);
+
   return (
     <>
       <PageTitle>Ingresso e pagamento</PageTitle>
-      {/* <PageSubtitle>Primeiro, escolha sua modalidade de ingresso</PageSubtitle>
-      <Options>
-        <Option>
-          <Type>Presencial</Type>
-          <Price>R$250</Price>
-        </Option>
-        <Option>
-          <Type>Online</Type>
-          <Price>R$100</Price>
-        </Option>
-      </Options> */}
-      <Center>
-        <BlockedText>Você precisa completar sua inscrição antes de prosseguir pra escolha de ingresso</BlockedText>
-      </Center>
+      {hasSubscription ?
+        <>
+          <PageSubtitle>Primeiro, escolha sua modalidade de ingresso</PageSubtitle>
+          <Options>
+            <Option>
+              <Type>Presencial</Type>
+              <Price>R$250</Price>
+            </Option>
+            <Option>
+              <Type>Online</Type>
+              <Price>R$100</Price>
+            </Option>
+          </Options>
+        </> :
+        <Center>
+          {loading ? <Loading /> : <BlockedText>Você precisa completar sua inscrição antes de prosseguir pra escolha de ingresso</BlockedText>}
+        </Center>}
     </>
   );
 }
