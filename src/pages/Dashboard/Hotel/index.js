d732eb61963ca8e7ea98eacde5596cc26963a5e4
Erick Silva <erickrsilva@hotmail.com>
2022-01-20 11:20:01 -0300

    Hotels rendering based on mocked data implemented

--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -1,3 +1,49 @@
+import HotelCard from "../../../components/HotelCard";
 export default function Hotel() {
-  return "Hotel: Em breve!";
+  return (
+    mockedHotelsData.map((hotel) => {
+      return (<HotelCard key={hotel.name}/>);
+    })
+  );
 }
+
+const mockedHotelsData = [
+  {
+    id: 1,
+    name: "Driven Resort",
+    imageUrl: "https://media-cdn.tripadvisor.com/media/photo-s/16/1a/ea/54/hotel-presidente-4s.jpg",
+    accommodationTypes: "Single e Double",
+    vacancies: 15,
+    rooms: [
+      {
+        id: 1,
+        hotelId: 1,
+        number: 101,
+        vacancies: [
+          {
+            id: 1,
+            roomId: 1,
+            isAvailable: true
+          },
+          {
+            id: 2,
+            roomId: 1,
+            isAvailable: false
+          }
+        ]
+      },
+      {
+        id: 2,
+        hotelId: 1,
+        number: 102,
+        vacancies: [
+          {
+            id: 3,
+            roomId: 1,
+            isAvailable: true
+          }
+        ]
+      }
+    ]
+  }
+];
