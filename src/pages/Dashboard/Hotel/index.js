c6e9981330c9ae8b87621bab87d5a60ea3b37450
Erick Silva <erickrsilva@hotmail.com>
2022-01-20 01:48:24 -0300

    Access denied to hotels based on payment - layout done

--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -1,3 +1,30 @@
+import { useState } from "react";
+import Loading from "../../../components/Loading";
+import {
+  PageTitle,
+  PageSubtitle,
+  Center,
+  BlockedText,
+} from "../../../components/DefaultTabStyle";
+
 export default function Hotel() {
-  return "Hotel: Em breve!";
+  const [hasPayment, setHasPayment] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <>
+      <PageTitle>Escolha de hotel e quarto</PageTitle>
+      {
+        hasPayment 
+          ?
+          <>
+            <PageSubtitle>Primeiro, escolha seu hotel</PageSubtitle>
+          </> 
+          :
+          <Center>
+            {loading ? <Loading /> : <BlockedText>Você precisa ter confirmado o pagamento antes de fazer a escolha de hospedagem</BlockedText>}
+          </Center>
+      }
+    </>
+  );
 }
