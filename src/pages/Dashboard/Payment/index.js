86f3f45cbbea43dbff1f545dc5f31f91af475e43
okitauehara <marcosokita@live.com>
2022-01-20 11:53:51 -0300

    paymentSelection: forking hotel selection into a new component

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,17 +1,9 @@
 import { useState, useEffect } from "react";
 import useApi from "../../../hooks/useApi";
 import Loading from "../../../components/Loading";
-import {
-  PageTitle,
-  PageSubtitle,
-  Options,
-  Option,
-  Type,
-  Price,
-  Center,
-  BlockedText,
-} from "../../../components/DefaultTabStyle";
+import { PageTitle, Center, BlockedText } from "../../../components/DefaultTabStyle";
 import TicketSelection from "./TicketSelection";
+import HotelSelection from "./HotelSelection";
 
 export default function Payment() {
   const [hasSubscription, setHasSubscription] = useState("");
@@ -44,23 +36,11 @@ export default function Payment() {
             setTicket={setTicket}
             setHasHotel={setHasHotel}
           />
-          <PageSubtitle visible={ticket === "presential" ? true : false}>Ótimo! Agora escolha sua modalidade de hospedagem</PageSubtitle>
-          <Options visible={ticket === "presential" ? true : false}>
-            <Option
-              selected={hasHotel === "no" ? true : false}
-              onClick={() => hasHotel === "no" ? setHasHotel(null) : setHasHotel("no")}
-            >
-              <Type>Sem Hotel</Type>
-              <Price>+ R$ 0</Price>
-            </Option>
-            <Option
-              selected={hasHotel === "yes" ? true : false}
-              onClick={() => hasHotel === "yes" ? setHasHotel(null) : setHasHotel("yes")}
-            >
-              <Type>Com Hotel</Type>
-              <Price>+ R$ 350</Price>
-            </Option>
-          </Options>
+          <HotelSelection
+            ticket={ticket}
+            hasHotel={hasHotel}
+            setHasHotel={setHasHotel}
+          />
         </> :
         <Center>
           {loading ? <Loading /> : <BlockedText>Você precisa completar sua inscrição antes de prosseguir pra escolha de ingresso</BlockedText>}
