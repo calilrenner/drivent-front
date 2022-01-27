88366499387bec45c54f84156cd857b57470e2a5
Erick Silva <erickrsilva@hotmail.com>
2022-01-27 13:31:03 -0300

    Activities rendering done

---
+++ src/pages/Dashboard/Activities/DayTrails.js
@@ -0,0 +1,156 @@
+import styled from "styled-components";
+import { BiLogIn } from "react-icons/bi";
+import { AiOutlineCloseCircle } from "react-icons/ai";
+import { Fade } from "@material-ui/core";
+
+export default function DayTrails({ dayTrails }) {
+  return(
+    <TrailsContainerStyle>
+      {
+        dayTrails.map((trail) => {
+          return (
+            <EventTrails
+              id={trail.id}
+              key={trail.trailName}
+              trailName={trail.trailName}
+              events={trail.events}
+            />
+          );
+        })
+      }
+    </TrailsContainerStyle>
+  );
+}
+
+function EventTrails({ id, trailName, events }) {
+  const animationIn = true;
+  return (
+    <TrailContainerStyle>
+      <TitleStyle>
+        <span>{trailName}</span>
+      </TitleStyle>
+      <EventContainerStyle>
+        {
+          events.map((event, index) => {
+            const animationTiming = (index + 1) * 2000;
+            return (
+              <Fade in={animationIn} timeout={{ enter: animationTiming, exit: animationTiming }}>
+                <EventCardStyle duration={event.duration}>
+                  <EventDescrpiptionStyle>
+                    <EventTitleStyle>{event.name}</EventTitleStyle>
+                    <EventTimeStyle>{event.startTime} - {event.endTime}</EventTimeStyle>
+                  </EventDescrpiptionStyle>
+                  <SeparatorStyle/>
+                  <VacanciesStyle>
+                    { 
+                      event.vacancies > 0
+                        ?
+                        <VacanciesAvailabilityStyle color='#078632' isAvailable={true}>
+                          <AvailableIcon/>
+                          <div>{event.vacancies} vagas</div>
+                        </VacanciesAvailabilityStyle>
+                        :
+                        <VacanciesAvailabilityStyle color='#CC6666' isAvailable={false}>
+                          <UnavailableIcon/>
+                          <div>Esgotado</div>
+                        </VacanciesAvailabilityStyle>
+                    }
+                  </VacanciesStyle>
+                </EventCardStyle>
+              </Fade>
+            );
+          })
+        }
+      </EventContainerStyle>
+    </TrailContainerStyle>
+  );
+}
+
+const TrailsContainerStyle = styled.section`
+  width: 100%;
+  margin-top: 40px;
+  display: flex;
+  font-family: 'Roboto', sans-serif;
+`;
+
+const TrailContainerStyle = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const TitleStyle = styled.div`
+  width: 100%;
+  font-size: 17px;
+  color: #7B7B7B;
+  display: flex;
+  justify-content: center;
+  padding: 13px 0 13px 0;
+`;
+
+const EventContainerStyle = styled.div`
+  width: 100%;
+  height: 350px;
+  padding: 14px;
+  border: 1px solid #D7D7D7;
+`;
+
+const EventCardStyle = styled.div`
+  width: 100%;
+  height: ${(props) => Math.floor(props.duration) === props.duration ? (props.duration*90)-10: props.duration*90}px;
+  background-color: #F1F1F1;
+  padding: 15px 10px 10px 10px;
+  border-radius: 5px;
+  margin-bottom: 10px;
+
+  display: flex;
+`;
+
+const EventDescrpiptionStyle = styled.div`
+  width: 70%;
+`;
+const EventTitleStyle = styled.div`
+  font-size: 12px;
+  font-weight: 700;
+  color: #343434;
+`;
+const EventTimeStyle = styled.div`
+  margin-top: 5px;
+  font-size: 12px;
+  color: #343434;
+`;
+
+const SeparatorStyle = styled.div`
+  width: 1px;
+  height: 100%;
+  background-color: #CFCFCF;
+`;
+
+const VacanciesStyle = styled.div`
+  width: 30%;
+  padding-left: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const VacanciesAvailabilityStyle = styled.div`
+  text-align: center;
+  font-size: 9px;
+  color: ${(props) => props.color};
+
+  :hover{
+    cursor: ${(props) => props.isAvailable ? "pointer" : "not-allowed"};
+    filter: ${(props) => props.isAvailable ? "brightness(1.2)" : "brightness(1)"};
+  }
+`;
+
+const AvailableIcon = styled(BiLogIn)`
+  font-size: 18px;
+`;
+
+const UnavailableIcon = styled(AiOutlineCloseCircle)`
+  font-size: 18px;
+`;
--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -1,3 +1,200 @@
+/* eslint-disable no-console */
+import styled from "styled-components";
+import { useEffect, useState, useContext } from "react";
+import { PageTitle, PageSubtitle, Center, BlockedText } from "../../../components/DefaultTabStyle";
+import Button from "../../../components/Form/Button";
+import useApi from "../../../hooks/useApi";
+import Loading from "../../../components/Loading";
+import UserContext from "../../../contexts/UserContext";
+import DayTrails from "./DayTrails";
+
 export default function Activities() {
-  return "Atividades: Em breve!";
+  const weekdays = [
+    {
+      id: 1,
+      day: "Sexta, 22/10",
+      isSelected: false,
+    },
+    {
+      id: 2,
+      day: "Sábado, 23/10",
+      isSelected: false,
+    },
+    {
+      id: 3,
+      day: "Domingo, 24/10",
+      isSelected: false,
+    }
+  ];
+
+  const [days, setDays] = useState(weekdays);
+  const [loading, setLoading] = useState(false);
+  const [hasPayment, setHasPayment] = useState(false);
+  const [isPresential, setIsPresential] = useState(false);
+  const [isDayOneSelected, setIsDayOneSelected] = useState(false);
+
+  const { userData } = useContext(UserContext);
+  const userId = userData.user.id;
+  const { payment } = useApi();
+
+  useEffect(() => {
+    setLoading(true);
+    payment.findPayment(userId)
+      .then((response) => {
+        if (response.status === 200) {
+          setHasPayment(true);
+          response.data[0].modalityId === 1 ? setIsPresential(true) : setIsPresential(false);
+        }
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+        setLoading(false);
+      });
+  }, [hasPayment, isPresential]);
+
+  const selectDay = (event) => {
+    const newArray = weekdays.map((d, index) => {
+      if (d.id.toString() === event.currentTarget.id) {
+        if(index === 0) setIsDayOneSelected(true);
+        else setIsDayOneSelected(false);
+        return { ...d, isSelected: true };
+      } else {
+        return { ...d, isSelected: false };
+      }
+    });
+    setDays(newArray);
+  };
+
+  return (
+    <>
+      <PageTitle>Escolha de atividades</PageTitle>
+      {
+        hasPayment && isPresential
+          ?
+          <>
+            <PageSubtitle visible={true}>Primeiro, filtre pelo dia do evento: </PageSubtitle>
+            <Weekdays>
+              {days.map((d) => {
+                return (
+                  <Day
+                    key={d.id}
+                    id={d.id}
+                    onClick={selectDay}
+                    selected={d.isSelected}
+                  >{d.day}</Day>
+                );
+              })}
+            </Weekdays>
+            {
+              isDayOneSelected 
+                ? 
+                <DayTrails dayTrails={mockedActivitiesDay1}/>
+                : 
+                <div/>}
+          </>
+          :
+          <Center>
+            {
+              loading
+                ?
+                <Loading />
+                :
+                <BlockedText>
+                  {
+                    hasPayment
+                      ?
+                      "Sua modalidade de ingresso não necessita escolher atividade. Você terá acesso a todas as atividades"
+                      :
+                      "Você precisa ter confirmado pagamento antes de fazer a escolha de atividades"
+                  }
+                </BlockedText>
+            }
+          </Center>       
+      }
+    </>
+  );
 }
+
+const Weekdays = styled.form`
+  width: fit-content;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  column-gap: 18px;
+`;
+
+const Day = styled(Button)`
+  max-width: 150px;
+  text-transform: none !important;
+  background-color: ${(props) => props.selected ? "#FFD37D !important" : ""}
+`;
+
+const mockedActivitiesDay1 = [
+  {
+    id: 1,
+    trailName: "Auditório Principal",
+    events: [
+      {
+        id: 1,
+        name: "Palestra A",
+        startTime: "09:00",
+        endTime: "10:00",
+        duration: 1,
+        vacancies: 15
+      },
+      {
+        id: 2,
+        name: "Palestra B",
+        startTime: "10:00",
+        endTime: "11:00",
+        duration: 1,
+        vacancies: 0
+      },
+      {
+        id: 3,
+        name: "Palestra C",
+        startTime: "11:00",
+        endTime: "12:00",
+        duration: 1,
+        vacancies: 15
+      }
+
+    ]
+  },
+  {
+    id: 2,
+    trailName: "Auditório Lateral",
+    events: [
+      {
+        id: 4,
+        name: "Palestra D",
+        startTime: "09:00",
+        endTime: "10:30",
+        duration: 1.5,
+        vacancies: 15
+      }
+    ]
+  },
+  {
+    id: 3,
+    trailName: "Workroom",
+    events: [
+      {
+        id: 5,
+        name: "Palestra E",
+        startTime: "09:00",
+        endTime: "11:00",
+        duration: 2,
+        vacancies: 15
+      },
+      {
+        id: 6,
+        name: "Palestra F",
+        startTime: "11:00",
+        endTime: "12:00",
+        duration: 1,
+        vacancies: 15
+      }
+    ]
+  },
+];
