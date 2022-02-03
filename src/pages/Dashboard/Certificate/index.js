8ad620dafdd12acc4e922985107ad776222dc15e
Erick Silva <erickrsilva@hotmail.com>
2022-02-02 21:40:23 -0300

    Access to certificate denied before payment

--- src/pages/Dashboard/Certificate/index.js
+++ src/pages/Dashboard/Certificate/index.js
@@ -1,7 +1,49 @@
+import { useContext, useEffect, useState } from "react";
+import { BlockedText, Center, PageTitle } from "../../../components/DefaultTabStyle";
+import Loading from "../../../components/Loading";
+import UserContext from "../../../contexts/UserContext";
+import useApi from "../../../hooks/useApi";
 import Certification from "./Certification";
 
 export default function Certificate() {
+  const { payment } = useApi();
+  const { userData } = useContext(UserContext);
+  const userId = userData.user.id;
+
+  const [hasPayment, setHasPayment] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  function checkPayment(userId) {
+    setLoading(true);
+    payment.findPayment(userId)
+      .then((response) => {
+        if (response.status === 200) {
+          setHasPayment(true);
+          setLoading(false);
+        }
+      })
+      .catch((error) => {
+        setLoading(false);
+        console.error(error);
+      });
+  }
+
+  useEffect(() => {
+    checkPayment(userId);
+  }, []);
+
   return (
-    <Certification/>
+    <>
+      <PageTitle>Certificado</PageTitle>
+      {
+        hasPayment
+          ?
+          <Certification/>
+          :
+          <Center>
+            {loading ? <Loading /> : <BlockedText>Você precisa participar do evento para obter um certificado</BlockedText>}
+          </Center>
+      }
+    </>
   );
 }
